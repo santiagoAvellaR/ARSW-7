68bfa72941f54fc562f9efcbf039d980a0ca8b00
Miguel Motta <93295363+MIGUEL-MOTTA-U@users.noreply.github.com>
2025-03-06 08:28:43 -0500

    feat: view structure and updating dependence of NextUI

--- blueprints-app/app/routes/_index.tsx
+++ blueprints-app/app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import {HeroUIProvider} from "@heroui/system";
+
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,8 +11,36 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      
-    </div>
+    //<div className="flex h-screen items-center justify-center">
+    <HeroUIProvider>
+      <div className="flex flex-col h-screen">
+        {/* Header */}
+        <header className="bg-gray-200 p-4">
+          <h1 className="text-xl font-bold">Header</h1>
+        </header>
+
+        {/* Body con dos columnas */}
+        <main className="flex flex-grow">
+          {/* Parte Izquierda */}
+          <section className="w-1/2 bg-blue-100 p-4">
+            <div className="flex justify-center">
+
+              </div>
+            <p>Contenido Izquierdo</p>
+          </section>
+
+          {/* Parte Derecha */}
+          <section className="w-1/2 bg-blue-200 p-4">
+            <p>Contenido Derecho</p>
+          </section>
+        </main>
+
+        {/* Footer */}
+        <footer className="bg-gray-200 p-4">
+          <p>Footer</p>
+        </footer>
+      </div>
+    </HeroUIProvider>
+//    </div>
   );
 }
